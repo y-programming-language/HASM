a400b7abe8924668740e63c511dbffd690fc210b
firelabs-io <firelabs-io@gmail.com>
2025-01-03 18:38:20 -0300

    moved to compile (uses '-o')

--- hasm.sh
+++ hasm.sh
@@ -2,6 +2,10 @@
 
 if [ "$1" == "test" ]; then
     python3 test/test.py
+elif [ "$1" == "-o" ]; then
+    python3 compiler.py "$2"
 else
-    python3 compiler.py "$1"
+    echo "provide argument"
+    echo "hasm -o file to compile file"
+    echo "test to test all feactures"
 fi
